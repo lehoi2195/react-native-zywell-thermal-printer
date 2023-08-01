985471b959912aff03d2c4567030e0a182a6bb2c
hoi.le <hoi.le.van@sobanhang.com>
2023-08-01 09:33:08 +0700

    fix: fix ios ImageTranfer.h

--- ios/ImageTranster.h
+++ ios/ImageTranster.h
@@ -11,37 +11,42 @@
 @interface ImageTranster : NSObject
 
 typedef enum {
-    Dithering=0,//图片单色处理的方式：二值法
-    Threshold//抖动算法
+  Dithering = 0, // 图片单色处理的方式：二值法
+  Threshold      // 抖动算法
 } BmpType;
 
 typedef enum {
-    RasterNolmorWH=0,//打印光栅位图的模式：正常大小
-    RasterDoubleWidth,//倍宽
-    RasterDoubleHeight,//倍高
-    RasterDoubleWH//倍宽高
+  RasterNolmorWH = 0, // 打印光栅位图的模式：正常大小
+  RasterDoubleWidth,  // 倍宽
+  RasterDoubleHeight, // 倍高
+  RasterDoubleWH      // 倍宽高
 } PrintRasterType;
-
-
-
 
 /// Convert the picture to grayscale and then into printer format data
 /// @param mImage The picture to be converted
 /// @param bmptype Image conversion algorithm type
-+(NSData *)Imagedata:(UIImage *) mImage andType:(BmpType) bmptype;
++ (NSData *)Imagedata:(UIImage *)mImage andType:(BmpType)bmptype;
 /// Convert pictures to raster bitmap format
 /// @param mIamge The picture to be converted
 /// @param bmptype Image conversion algorithm type
 /// @param type The type of picture print size
-+(NSData *)rasterImagedata:(UIImage *) mIamge andType:(BmpType) bmptype andPrintRasterType:(PrintRasterType) type;
++ (NSData *)rasterImagedata:(UIImage *)mIamge
+                    andType:(BmpType)bmptype
+         andPrintRasterType:(PrintRasterType)type;
++ (UIImage *)imageCompressForWidthScale:(UIImage *)sourceImage
+                            targetWidth:(CGFloat)defineWidth;
 
-+(uint8_t *)imgToGreyImage:(UIImage *)image;
-+(UIImage *)imgWithImage:(UIImage *)image scaledToFillSize:(CGSize)size;
-+(NSData *)imgBitmapToArray:(UIImage*) bmp;
-+(NSData *)convertEachLinePixToCmd:(unsigned char *)src nWidth:(NSInteger) nWidth nHeight:(NSInteger) nHeight nMode:(NSInteger) nMode;
-+(unsigned char *)img_format_K_threshold:(unsigned char *) orgpixels
-                               width:(NSInteger) xsize height:(NSInteger) ysize;
-+(NSData *)imgPixToTscCmd:(uint8_t *)src width:(NSInteger) width;
-+(UIImage*)imgPadLeft:(NSInteger) left withSource: (UIImage*)source;
++ (uint8_t *)imgToGreyImage:(UIImage *)image;
++ (UIImage *)imgWithImage:(UIImage *)image scaledToFillSize:(CGSize)size;
++ (NSData *)imgBitmapToArray:(UIImage *)bmp;
++ (NSData *)convertEachLinePixToCmd:(unsigned char *)src
+                             nWidth:(NSInteger)nWidth
+                            nHeight:(NSInteger)nHeight
+                              nMode:(NSInteger)nMode;
++ (unsigned char *)img_format_K_threshold:(unsigned char *)orgpixels
+                                    width:(NSInteger)xsize
+                                   height:(NSInteger)ysize;
++ (NSData *)imgPixToTscCmd:(uint8_t *)src width:(NSInteger)width;
++ (UIImage *)imgPadLeft:(NSInteger)left withSource:(UIImage *)source;
 
 @end
